f549ca4cf039878034f82b95573223bbdefdee24
TonyB <63916873+amercat@users.noreply.github.com>
2026-03-21 11:16:39 +0100

    Correct string name check for main execution
    Fix typo in conditional check for main execution.

--- mathlafd.py
+++ mathlafd.py
@@ -5,8 +5,8 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
 # I'd add this to the file 
-#if __string__ == '__main__':
-#   unittest.main()
+if __string__ == '__main__':
+    unittest.main()
 
 if __name__ == '__main__':
     unittest.main()
